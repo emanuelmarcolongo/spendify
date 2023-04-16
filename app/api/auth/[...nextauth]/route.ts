24e92efb53e033af857799262fa6b1724d9aec30
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2023-04-16 14:06:41 -0300

    test for deploy

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,9 +4,6 @@ import NextAuth, { type NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 export const authOptions: NextAuthOptions = {
-  pages: {
-    signIn: '/login'
-  },
   session: {
     strategy: 'jwt'
   },
