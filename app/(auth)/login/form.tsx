c0250b3c72ecc20d05ea503fe9ddd9eb77b9a55a
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2023-04-21 12:44:25 -0300

    Revert "add env variable for signIn with credentials"
    This reverts commit a3156fee90ada69ff54f53e7fb66f3b5af04f849.

--- app/(auth)/login/form.tsx
+++ app/(auth)/login/form.tsx
@@ -29,16 +29,16 @@ export default function LoginForm() {
   async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     setError("")
-    
+
     try {
       const res = await signIn("credentials", {
         redirect: false,
         email: form.email,
         password: form.password,
-        callbackUrl: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/dashboard`
+        callbackUrl: `/dashboard`
       });
       if (!res?.error) {
-        router.push(`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/dashboard`)
+        router.push(`/dashboard`)
       } else {
         setError('Email ou senha inválidos')
       }
