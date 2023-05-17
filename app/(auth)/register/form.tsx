863f1ea0ede5a07073f96dd2c04848cdf6266f07
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2023-05-17 17:18:41 -0300

    feat: add react-hook-form in register page

--- app/(auth)/register/form.tsx
+++ app/(auth)/register/form.tsx
@@ -6,77 +6,116 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 export default function SignUpForm() {
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
+  const [error, setError] = useState<string | null>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   });
 
-  const [error, setError] = useState<string | null>(null)
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    setError("");
 
-  function handleForm(e: React.ChangeEvent<HTMLInputElement>) {
-    setForm({
-      ...form,
-      [e.currentTarget.name]: e.currentTarget.value,
-    });
-  }
-
-  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    setError("")
-
-    if (form.password !== form.confirmPassword) {
-      return setError('As senhas devem ser iguais')
+    if (data.password !== data.confirmPassword) {
+      return setError("As senhas devem ser iguais");
     }
 
     try {
-      const res = await fetch('/api/register', {
-        method: 'POST',
-        body: JSON.stringify(form),
+      const res = await fetch("/api/register", {
+        method: "POST",
+        body: JSON.stringify(data),
         headers: {
-          'Content-Type': 'application/json'
-        }
-      })
+          "Content-Type": "application/json",
+        },
+      });
       if (res.ok) {
-        signIn()
-      }
-      else {
-        setError((await res.json()).error)
+        signIn();
+      } else {
+        setError((await res.json()).error);
       }
     } catch (error: any) {
-      setError(error?.message)
+      setError(error?.message);
     }
-  }
+  };
 
   return (
-    <form onSubmit={onSubmit} className="space-y-8 w-full sm:w-[400px]">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="space-y-8 w-full sm:w-[400px]"
+    >
       <div className="grid w-full gap-2 items-center">
         <Label htmlFor="name">Nome completo</Label>
-        <Input required value={form.name} onChange={handleForm} name='name' type='text'></Input>
+        <Input
+          required
+          {...register("name", {
+            required: true
+          })}
+          name="name"
+          type="text"
+        ></Input>
       </div>
 
       <div className="grid w-full  items-center">
         <Label htmlFor="email">E-mail</Label>
-        <Input required value={form.email} onChange={handleForm} name='email' type='email'></Input>
+        <Input
+          required
+          {...register("email", {
+            required: true,
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Insira um formato de e-mail válido",
+            },
+          })}
+          name="email"
+          type="email"
+        ></Input>
       </div>
 
       <div className="grid w-full items-center">
         <Label htmlFor="password">Senha</Label>
-        <Input required value={form.password}  onChange={handleForm} name='password' type='password'></Input>
+        <Input
+          required
+          {...register("password", {
+            required: true
+          })}
+          name="password"
+          type="password"
+        ></Input>
       </div>
 
       <div className="grid w-full items-center">
         <Label htmlFor="password">Confirmar senha</Label>
-        <Input required value={form.confirmPassword}  onChange={handleForm} name='confirmPassword' type='password'></Input>
+        <Input
+          required
+          {...register("confirmPassword", {
+            required: true
+          })}
+          name="confirmPassword"
+          type="password"
+        ></Input>
       </div>
 
       {error && <Alert>{error}</Alert>}
 
       <div className="w-full">
-      <Button className="w-full ">Criar conta!</Button>
+        <Button type='submit' className="w-full ">Criar conta!</Button>
       </div>
     </form>
   );
