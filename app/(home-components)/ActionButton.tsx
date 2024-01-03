a7b38032373c2fe6c28fb120daab764cbec7b0e0
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2024-01-03 20:45:36 -0300

    refactor: Action button wrapped by <a> to correct redirection

--- app/(home-components)/ActionButton.tsx
+++ app/(home-components)/ActionButton.tsx
@@ -7,9 +7,11 @@ type ActionButtonProps = {
 
 const ActionButton = ({ innerText, navigationRef }: ActionButtonProps) => {
   return (
-    <Button className="rounded-full opacity-80 mt-20 h-[50px] min-w-[200px] mb-20">
-      <a href={navigationRef}>{innerText}</a>
-    </Button>
+    <a href={navigationRef}>
+      <Button className="rounded-full opacity-80 mt-20 h-[50px] min-w-[200px] mb-20">
+        {innerText}
+      </Button>
+    </a>
   );
 };
 
